d06581ed304dc7ce24a28d9a11078822ea146673
Lucas <lucas@War-Machine.local>
2013-01-06 20:36:12 -0200

    Initial Commit

---
+++ Cocoa+Extensions/Cocoa_Extensions.h
@@ -0,0 +1,13 @@
+//
+//  Cocoa_Extensions.h
+//  Cocoa+Extensions
+//
+//  Created by Lucas on 1/6/13.
+//  Copyright (c) 2013 Lucas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cocoa_Extensions : NSObject
+
+@end
---
+++ Cocoa+Extensions/Cocoa_Extensions.m
@@ -0,0 +1,13 @@
+//
+//  Cocoa_Extensions.m
+//  Cocoa+Extensions
+//
+//  Created by Lucas on 1/6/13.
+//  Copyright (c) 2013 Lucas. All rights reserved.
+//
+
+#import "Cocoa_Extensions.h"
+
+@implementation Cocoa_Extensions
+
+@end
