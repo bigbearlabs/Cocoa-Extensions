7d05a376839f5ff2c5bb4b0bf0d19810267896e9
Lucas <lucas@War-Machine.local>
2013-01-06 23:54:21 -0200

    Edit project url
    Correct all the wrong url

--- Cocoa+Extensions/NSString+URL.m
+++ Cocoa+Extensions/NSString+URL.m
@@ -3,7 +3,7 @@
 //  Cocoa+Extensions
 //
 //  Created by iLucas.
-//  http://github.com/ilucas/Cocoa+Extensions
+//  http://github.com/ilucas/Cocoa-Extensions
 //
 //  Copyright (c) 2012-2013. All rights reserved.
 //  Licensed under the New BSD License
